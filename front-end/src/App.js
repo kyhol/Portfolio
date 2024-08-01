330f81b6dd00068197534bbe90968f031148e87c
kyhol <Kyle.Hollett@keyin.com>
2024-08-01 14:26:19 -0230

    Started react-router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
